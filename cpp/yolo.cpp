e2583a6f023175f46e70ebd7477e128540ea61e1
molnarm <martin.molnar@zelektronika.eu>
2024-05-21 12:56:54 +0200

    test_out.mp4 hozzaadva

--- cpp/yolo.cpp
+++ cpp/yolo.cpp
@@ -36,11 +36,11 @@ const float CONFIDENCE_THRESHOLD = 0.06;
  *
  * @return A classok neveit tartalmazó string lista.
  */
-std::vector<std::string> load_class_list()
+vector<string> load_class_list()
 {
-    std::vector<std::string> class_list;
-    std::ifstream ifs("config_files/v4.txt"); // Fájl megnyitása
-    std::string line;
+    vector<string> class_list;
+    ifstream ifs("config_files/v4.txt"); // Fájl megnyitása
+    string line;
     while (getline(ifs, line)) // Soronként olvasás
     {
         class_list.push_back(line); // Sor hozzáadása a listához
@@ -59,20 +59,20 @@ void load_net(dnn::Net &net, bool is_cuda)
     auto result = dnn::readNet("config_files/v4.onnx");
     if (is_cuda)
     {
-        std::cout << "Cuda magok használata\n";
+        cout << "Cuda magok használata\n";
         result.setPreferableBackend(dnn::DNN_BACKEND_CUDA);
         result.setPreferableTarget(dnn::DNN_TARGET_CUDA_FP16);
     }
     else
     {
-        std::cout << "CPU használata\n";
+        cout << "CPU használata\n";
         result.setPreferableBackend(dnn::DNN_BACKEND_OPENCV);
         result.setPreferableTarget(dnn::DNN_TARGET_CPU);
     }
     net = result;
 }
 
-const std::vector<Scalar> colors = {Scalar(255, 255, 0), Scalar(0, 255, 0), Scalar(0, 255, 255), Scalar(255, 0, 0)};
+const vector<Scalar> colors = {Scalar(255, 255, 0), Scalar(0, 255, 0), Scalar(0, 255, 255), Scalar(255, 0, 0)};
 
 /**
  * Formázza az input képet a YOLOv5 modell számára.
@@ -97,30 +97,32 @@ Mat format_yolov5(const Mat &source) {
  * @param output detekt lista
  * @param className osztálynevek listája
  */
-void detect(Mat &image, dnn::Net &net, std::vector<Detection> &output, const std::vector<std::string> &className) {
+void detect(Mat &image, dnn::Net &net, vector<Detection> &output, const vector<string> &className) {
     Mat blob;
 
     auto input_image = format_yolov5(image); 
     dnn::blobFromImage(input_image, blob, 1./255., Size(INPUT_WIDTH, INPUT_HEIGHT), Scalar(), true, false); 
     net.setInput(blob); 
-    std::vector<Mat> outputs;
+    vector<Mat> outputs;
     net.forward(outputs, net.getUnconnectedOutLayersNames()); 
+
 
     float x_factor = input_image.cols / INPUT_WIDTH;
     float y_factor = input_image.rows / INPUT_HEIGHT;
     
     float *data = (float *)outputs[0].data; 
-
-    const int dimensions = 6; 
-    const int rows = 25200; 
+  
     
-    std::vector<int> class_ids;
-    std::vector<float> confidences;
-    std::vector<Rect> boxes;
+    const int dimensions = 6; 
+    const int rows = 25200; //A neurális hálóban lévő három concatból adódik össze 19200 + 4800 + 1200
+    
+    vector<int> class_ids;
+    vector<float> confidences;
+    vector<Rect> boxes;
 
     for (int i = 0; i < rows; ++i) {
-
         float confidence = data[4];
+        cout << data << ": " << confidence << endl;
         if (confidence >= CONFIDENCE_THRESHOLD) {
 
             float * classes_scores = data + 5; 
@@ -145,11 +147,12 @@ void detect(Mat &image, dnn::Net &net, std::vector<Detection> &output, const std
                 boxes.push_back(Rect(left, top, width, height)); 
             }
 
+      
         }
-        data +=6; 
+        data +=6; //Következő memoria címre ugrunk
 
     }
-    std::vector<int> nms_result;
+    vector<int> nms_result;
     dnn::NMSBoxes(boxes, confidences, SCORE_THRESHOLD, NMS_THRESHOLD, nms_result); 
     for (int i = 0; i < nms_result.size(); i++) {
         int idx = nms_result[i];
@@ -171,7 +174,7 @@ int main(int argc, char **argv)
       gpsThread.detach();
     }
     
-    std::vector<std::string> class_list = load_class_list(); 
+    vector<string> class_list = load_class_list(); 
 
     printf("Input video: %s\n ", argv[1]);
     printf("Output video: %s\n ", argv[2]);
@@ -179,7 +182,7 @@ int main(int argc, char **argv)
     VideoCapture capture(argv[1]); 
     if (!capture.isOpened())
     {
-        std::cerr << "Hiba a videó fájl megnyitása közben:\n Biztos létezik ilyen?\n Lehet rossz a kódolás \n";
+        cerr << "Hiba a videó fájl megnyitása közben:\n Biztos létezik ilyen?\n Lehet rossz a kódolás \n";
         return -1;
     }
 
@@ -188,7 +191,7 @@ int main(int argc, char **argv)
     dnn::Net net;
     load_net(net, is_cuda); 
     capture.read(frame);
-    auto start = std::chrono::high_resolution_clock::now(); 
+    auto start = chrono::high_resolution_clock::now(); 
     int frame_count = 0;
     float fps = -1;
     int total_frames = 0;
@@ -197,8 +200,8 @@ int main(int argc, char **argv)
     
     VideoWriter video(argv[2], VideoWriter::fourcc('a','v','c','1'), 10, Size(frame_width,frame_height)); 
     
-    std::ofstream outfile;
-    outfile.open("log.csv", std::ios_base::app); 
+    ofstream outfile;
+    outfile.open("log.csv", ios_base::app); 
     outfile << "Weed Count;" << "GPS Lat;" << "GPS Lon \n"; 
     
     while (true)
@@ -207,11 +210,11 @@ int main(int argc, char **argv)
         capture.read(frame); 
         if (frame.empty())
         {
-            std::cout << "Vége a videónak\n";
+            cout << "Vége a videónak\n";
             break;
         }
 
-        std::vector<Detection> output;
+        vector<Detection> output;
         detect(frame, net, output, class_list); 
         
         frame_count++;
@@ -235,34 +238,34 @@ int main(int argc, char **argv)
         if (frame_count >= 30)
         {
 
-            auto end = std::chrono::high_resolution_clock::now(); 
-            fps = frame_count * 1000.0 / std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count(); 
+            auto end = chrono::high_resolution_clock::now(); 
+            fps = frame_count * 1000.0 / chrono::duration_cast<chrono::milliseconds>(end - start).count(); 
 
             frame_count = 0;
-            start = std::chrono::high_resolution_clock::now(); 
+            start = chrono::high_resolution_clock::now(); 
         }
 
         if (fps > 0)
         {
 
-            std::ostringstream fps_label;
-            fps_label << std::fixed << std::setprecision(2);
+            ostringstream fps_label;
+            fps_label << fixed << setprecision(2);
             fps_label << "FPS: " << fps;
-            std::string fps_label_str = fps_label.str();
+            string fps_label_str = fps_label.str();
 
-            std::ostringstream weed_sum_label;
-            weed_sum_label << std::fixed << std::setprecision(2);
+            ostringstream weed_sum_label;
+            weed_sum_label << fixed << setprecision(2);
             weed_sum_label << "Detected Weed: " << detections;
-            std::string weed_sum_label_str = weed_sum_label.str();
+            string weed_sum_label_str = weed_sum_label.str();
             
             if(gpsIsConnected){
                 GPSDataClass gpsData =  returnGPSData();
-                std::ostringstream gpsCor;
-                gpsCor << std::fixed << std::setprecision(2);
+                ostringstream gpsCor;
+                gpsCor << fixed << setprecision(2);
                 gpsCor << "GPS Coordinates  " << gpsData.lat << "  " <<gpsData.lon;
-                std::string gpsCor_str = gpsCor.str();
-                std::cout<< "GPS type: " << gpsData.type<<endl;
-                std::cout<< "GPS time: " << gpsData.time<<endl;
+                string gpsCor_str = gpsCor.str();
+                cout<< "GPS type: " << gpsData.type<<endl;
+                cout<< "GPS time: " << gpsData.time<<endl;
                 outfile << detections <<";"<< gpsData.lat <<";"<< gpsData.lon<<"\n"; 
                 putText(frame, gpsCor_str.c_str(), Point(10, 105), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 2); 
             }
@@ -289,12 +292,12 @@ int main(int argc, char **argv)
         if (waitKey(1) != -1)
         {
             capture.release();
-            std::cout << "befejezve \n";
+            cout << "befejezve \n";
             break;
         }
     }
 
-    std::cout << "Az összes frame ami fel lett dolgozva: " << total_frames << "\n"; 
+    cout << "Az összes frame ami fel lett dolgozva: " << total_frames << "\n"; 
     video.release(); 
     outfile.close(); 
     
